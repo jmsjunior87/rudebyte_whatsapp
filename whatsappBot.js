bd1b913cfa3c62e778412df3c302016693e794e2
jmsjunior87 <140747084+jmsjunior87@users.noreply.github.com>
2024-09-03 14:26:16 -0300

    Update whatsappBot.js

--- whatsappBot.js
+++ whatsappBot.js
@@ -60,6 +60,7 @@ app.post('/whatsapp', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
 });
